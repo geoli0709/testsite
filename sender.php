f61eb32da2d94f0dee922a8522f03fcafb18a367
geoli0709 <geoli0709@gmail.com>
2022-08-07 22:45:22 +0300

    com

--- sender.php
+++ sender.php
@@ -1,24 +1,24 @@
 <?php
-    $name = $_POST['name'];
-    $surname = $_POST['surname'];
-	$phone = $_POST['phone'];
-    $email = $_POST['email'];
-    $text = $_POST['text'];
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$text = $_POST['text'];
 
-	$to = "LIONSGRIP@yandex.ru"; 
-	$date = date ("d.m.Y"); 
-	$time = date ("h:i");
-	$from = $email;
-	$subject = "Заявка c сайта";
+$to = "LIONSGRIP@yandex.com";
+$date = date("d.m.Y");
+$time = date("h:i");
+$from = $email;
+$subject = "Заявка c сайта";
 
-	
-	$msg="
+
+$msg = "
     Имя: $name /n
     Фамилия: $surname /n
     Телефон: $phone /n
     Почта: $email /n
-    Текст: $text"; 	
-	mail($to, $subject, $msg, "From: $from ");
+    Текст: $text";
+mail($to, $subject, $msg, "From: $from ");
 
 ?>
 
